fc55b0354ea3b5a77f18d7004806e22fc99bcc9c
pakeeja <pakeeja07@gmail.com>
2021-02-20 19:46:51 +0530

    Day 1 Commit

---
+++ Day_1_Band_Name_generator.py
@@ -0,0 +1,5 @@
+
+print("Welcome to BAND NAME GENERATOR.")
+city=input("What's the name of the city you grew up in?\n")
+pet=input("What's your pet Name?\n")
+print("Your BAND name could be "+city+" "+pet)
